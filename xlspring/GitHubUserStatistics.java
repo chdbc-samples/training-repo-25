1cd6e41fee51be3e053d66a20cd3d8f79823d50d
xlspring <tsyomav@gmail.com>
2025-09-18 11:00:33 +0300

    Fix USER_NAME error in xlspring/GitHubUserStatistics.java

--- xlspring/GitHubUserStatistics.java
+++ xlspring/GitHubUserStatistics.java
@@ -5,8 +5,7 @@ import java.net.URL;
 
 
 public class GitHubUserStatistics {
-    public final static String USER_NAME;
-
+    public final static String USER_NAME = "xlspring";
 
     public static void main(String[] args) {
         String apiUrl = "https://api.github.com/users/" + USER_NAME;
